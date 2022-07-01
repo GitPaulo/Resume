8f2fdc870faabaa57ca0c32027f5bfa4b21af53d
GitPaulo <paulo.santos98@gmail.com>
2022-07-01 20:49:25 +0000

    improvements

--- js/main.js
+++ js/main.js
@@ -1,19 +1,18 @@
-console.log("Loading resume pfg...");
-
-const pdfjsLib    = require('pdfjs-dist');
+const pdfjsLib = require('pdfjs-dist');
 const loadingTask = pdfjsLib.getDocument('./resources/paulo_resume.pdf');
 
 loadingTask.promise.then((pdf) => {
     pdf.getPage(1).then((page) => {
-        let scale    = 1.5;
+        page.getAnnotations().then(console.log)
+        let scale = 1.5;
         let viewport = page.getViewport({
             scale: scale,
         });
 
-        let canvas    = document.getElementById('resume_canvas');
-        let context   = canvas.getContext('2d');
+        let canvas = document.getElementById('resume_canvas');
+        let context = canvas.getContext('2d');
         canvas.height = viewport.height;
-        canvas.width  = viewport.width;
+        canvas.width = viewport.width;
 
         let renderContext = {
             canvasContext: context,
