fe4faa0d37eb6788cdc92ebc829a003f15c4dcd1
Paulo Santos <GitPaulo@users.noreply.github.com>
2025-11-09 15:40:19 +0000

    update pages jekyll config

--- webpack.config.cjs
+++ webpack.config.cjs
@@ -11,7 +11,7 @@ module.exports = {
   mode: "none",
   output: {
     path: path.join(__dirname, "./build"),
-    publicPath: "build",
+    publicPath: "/build/",
     filename: "[name].js",
   },
   module: {
